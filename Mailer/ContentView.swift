30f3a4d8af0e531854d53589441fce47c6a9d56a
Zach Eriksen <zmeriksen@icloud.com>
2021-05-03 17:54:04 -0500

    Initial Commit

---
+++ Mailer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mailer
+//
+//  Created by Leif on 5/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mailer/MailerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailerApp.swift
+//  Mailer
+//
+//  Created by Leif on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MailerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
